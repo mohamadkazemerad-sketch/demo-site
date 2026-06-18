80deb92e07457b427c3f54d97fb6960db748099e
mohamadkazemerad-sketch <mohamadkazemerad@gmail.com>
2026-06-18 14:09:56 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function order(product){
+
+let size = document.getElementById("size").value;
+
+let message = 
+"سلام، سفارش دارم\n" +
+"محصول: " + product +
+"\nسایز: " + size;
+
+window.open(
+"https://rubika.ir/mohamad_kazeme"
+);
+
+}
